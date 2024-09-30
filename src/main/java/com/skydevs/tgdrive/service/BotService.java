01fd994953ac1c4238c658332ec9effe79fc362d
SkyDependence <nanyangzesi@gmail.com>
2024-09-30 23:47:24 +0800

    add：初始化tg bot

--- src/main/java/com/skydevs/tgdrive/service/BotService.java
+++ src/main/java/com/skydevs/tgdrive/service/BotService.java
@@ -2,11 +2,15 @@ package com.skydevs.tgdrive.service;
 
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.longpolling.interfaces.LongPollingUpdateConsumer;
+import org.telegram.telegrambots.longpolling.starter.SpringLongPollingBot;
+import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
+import org.telegram.telegrambots.meta.api.objects.Update;
 
 /**
  * 机器人服务类，用于初始化机器人和运行机器人相关服务
  */
-public interface BotService {
+public interface BotService extends LongPollingSingleThreadUpdateConsumer, SpringLongPollingBot {
     /**
      * start bot
      */
@@ -16,4 +20,20 @@ public interface BotService {
      * 模拟机器人任务
      */
     void BotDo();
+
+    /**
+     * 检查更新
+     * @param update
+     */
+    @Override
+    public void consume(Update update);
+    /**
+     * 获取bot token
+     * @return
+     */
+    @Override
+    public String getBotToken();
+
+    @Override
+    public LongPollingUpdateConsumer getUpdatesConsumer();
 }
