eec8f9fec9193286231df3f5966faab3c49a2554
SkyDependence <nanyangzesi@gmail.com>
2024-11-04 19:23:47 +0800

    feat: finish developing the configs mapper layer

--- src/main/java/com/skydevs/tgdrive/mapper/ConfigMapper.java
+++ src/main/java/com/skydevs/tgdrive/mapper/ConfigMapper.java
@@ -2,8 +2,23 @@ package com.skydevs.tgdrive.mapper;
 
 import com.skydevs.tgdrive.dto.ConfigForm;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface ConfigMapper {
+    /**
+     * 插入一个配置
+     * @param configForm
+     */
     void insert(ConfigForm configForm);
+
+
+    /**
+     * 根据配置名查询配置
+     * @param name
+     * @return
+     */
+    @Select("SELECT * From configs where name = #{name}")
+    ConfigForm getByName(@Param("name")String name);
 }
