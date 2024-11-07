7794688b0db22fa6abd22e54297490db73ae8624
SkyDependence <nanyangzesi@gmail.com>
2024-11-07 22:23:38 +0800

    fix: cannot download files that are not recorded in database

--- src/main/java/com/skydevs/tgdrive/service/impl/DownloadServiceImpl.java
+++ src/main/java/com/skydevs/tgdrive/service/impl/DownloadServiceImpl.java
@@ -40,7 +40,7 @@ public class DownloadServiceImpl implements DownloadService {
             // 从 botService 获取文件的下载路径和文件名
             String fileUrl = botService.getFullDownloadPath(fileID);
             String filename = fileMapper.getFileNameByFileId(fileID);
-            if (filename != null) {
+            if (filename == null) {
                 filename = botService.getFileNameByID(fileID);
             }
             // 上传到tg的gif会被转换为MP4
