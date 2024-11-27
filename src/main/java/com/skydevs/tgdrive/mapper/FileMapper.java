3bf8b5422f71a0bc7c9f70b5143a5afe8b826d51
SkyDependence <nanyangzesi@gmail.com>
2024-11-27 21:46:51 +0800

    fix: Improper naming

--- src/main/java/com/skydevs/tgdrive/mapper/FileMapper.java
+++ src/main/java/com/skydevs/tgdrive/mapper/FileMapper.java
@@ -13,7 +13,7 @@ public interface FileMapper {
      * 插入已上传文件
      * @param fileInfo
      */
-    @Insert("INSERT INTO files (file_name, download_url, upload_time, file_id, size, full_size) VALUES (#{fileName}, #{downloadUrl}, #{uploadTime}, #{fileId}, #{size}, #{full_size})")
+    @Insert("INSERT INTO files (file_name, download_url, upload_time, file_id, size, full_size) VALUES (#{fileName}, #{downloadUrl}, #{uploadTime}, #{fileId}, #{size}, #{fullSize})")
     void insertFile(FileInfo fileInfo);
 
     /**
@@ -25,4 +25,7 @@ public interface FileMapper {
 
     @Select("SELECT file_name FROM files where file_id = #{fileId}")
     String getFileNameByFileId(String fileId);
+
+    @Select("SELECT full_size FROM files where file_id = #{fileId}")
+    String getFullSizeByFileId(String fileId);
 }
