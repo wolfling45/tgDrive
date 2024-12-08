c1dce478d84c02aad08487a33114f88a7e66d939
SkyDependence <nanyangzesi@gmail.com>
2024-12-08 21:47:47 +0800

    fix: generate redundent port when use http and https

--- src/main/java/com/skydevs/tgdrive/service/impl/BotServiceImpl.java
+++ src/main/java/com/skydevs/tgdrive/service/impl/BotServiceImpl.java
@@ -384,6 +384,10 @@ public class BotServiceImpl implements BotService {
         String protocol = request.getHeader("X-Forwarded-Proto") != null ? request.getHeader("X-Forwarded-Proto") : request.getScheme(); // 先代理请求头中获取协议
         String host = request.getServerName(); // 获取主机名 localhost 或实际域名
         int port = request.getHeader("X-Forwarded-Port") != null ? Integer.parseInt(request.getHeader("X-Forwarded-Port")) : request.getServerPort(); // 先从代理请求头中获取端口号 8080 或其他
+        // 如果是默认端口，则省略端口号
+        if ((protocol.equalsIgnoreCase("http") && port == 80) || (protocol.equalsIgnoreCase("https") && port == 443)) {
+            return protocol + "://" + host;
+        }
         return protocol + "://" + host + ":" + port;
     }
 }
