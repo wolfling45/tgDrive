bd9afd46b7a615ed31b306a3a4036b66e1436e00
SkyDependence <nanyangzesi@gmail.com>
2025-03-18 21:25:20 +0800

    fix: 新项目启动出错

--- src/main/resources/db/migration/V6__AddVisitor.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO users (username, password, role)
-SELECT 'visitor', '96e79218965eb72c92a549dd5a330112', 'visitor'
-WHERE NOT EXISTS (
-    SELECT 1 FROM users WHERE username = 'visitor'
-);
